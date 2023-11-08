e88095c07f4efbe38404bdf8229a1d426fa5ffe3
Zoltai Tamás <tomi.zoltai@gmail.com>
2023-11-08 11:22:24 +0100

    stat_2023_11_08

--- main.py
+++ main.py
@@ -25,15 +25,20 @@ def statok():
         teljesitmeny = jol_atengedett / (jol_atengedett + rosszul_atengedett)*100
     else:
         teljesitmeny = 100
-    print('---------------------------------------------------------------------------------------------------------')
-    print(f'  pénz={penz}Euro      idő={int((ido-ido%60)/60)} óra {ido%60} perc      életkedv={eletkedv}       teljesítmény= {teljesitmeny:.2f}%      rang = {rangneve}')
-    print('---------------------------------------------------------------------------------------------------------\n')
-
+    
+    if ido%60 < 10:    
+        print('--------------------------------------------------------------------------------------------------')
+        print(f'  pénz = {penz}€      idő: {int((ido-ido%60)/60)}:0{ido%60}      kereset = {kereset}       teljesítmény = {teljesitmeny:.2f}%      rang: {rangneve}')
+        print('--------------------------------------------------------------------------------------------------\n')
+    else:
+        print('--------------------------------------------------------------------------------------------------')
+        print(f'  pénz = {penz}€      idő: {int((ido-ido%60)/60)}:{ido%60}      kereset = {kereset}       teljesítmény = {teljesitmeny:.2f}%      rang: {rangneve}')
+        print('--------------------------------------------------------------------------------------------------\n')
 
 while penz < 200:
     os.system('cls')
     ido = 480
-    penz -= 5
+    penz -= 11
     penz += kereset
 
     while ido < 1080:
